089e09da004bc26f844d92f444a8772c04638c9a
Reed Lambier <rlambier46@gmail.com>
2016-07-01 16:47:06 -0700

    will add log in and password Log TAG to console

--- app/src/main/java/com/example/guest/nr_studyapp/logInActivity.java
+++ app/src/main/java/com/example/guest/nr_studyapp/logInActivity.java
@@ -2,12 +2,40 @@ package com.example.guest.nr_studyapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import android.util.Log;
 
-public class logInActivity extends AppCompatActivity {
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class logInActivity extends AppCompatActivity implements View.OnClickListener{
+    public static final String TAG = logInActivity.class.getSimpleName();
+    @Bind(R.id.logInUserName) EditText mlogInUserName;
+    @Bind(R.id.logInButton) Button mlogInButton;
+    @Bind(R.id.passwordLogIn) EditText mpassWordLogIn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+
+        ButterKnife.bind(this);
+
+        mlogInButton.setOnClickListener(this);
     }
+
+    @Override
+    public void onClick(View v) {
+        if (v == mlogInButton) {
+            String user = mlogInUserName.getText().toString();
+            String password = mpassWordLogIn.getText().toString();
+            Log.d(TAG, user);
+            Log.d(TAG, password);
+            Toast.makeText(logInActivity.this, "check console", Toast.LENGTH_LONG).show();
+        }
+    }
+
 }
