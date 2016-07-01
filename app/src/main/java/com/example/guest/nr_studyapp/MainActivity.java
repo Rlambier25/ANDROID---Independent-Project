4290bd4f9da408fcca3c98b56b72f8f709f3edac
Reed Lambier <rlambier46@gmail.com>
2016-07-01 13:03:56 -0700

    Trouble shooting front page

--- app/src/main/java/com/example/guest/nr_studyapp/MainActivity.java
+++ app/src/main/java/com/example/guest/nr_studyapp/MainActivity.java
@@ -1,13 +1,25 @@
 package com.example.guest.nr_studyapp;
 
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+    @Bind(R.id.logInButton) Button mLogInButton;
+    @Bind(R.id. appNameTextView) TextView mAppNameTextView;
+    @Bind(R.id.aboutButton) Button mAboutButton;
+    @Bind(R.id.flashCardButton) Button mFlashCardButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        Typeface captureFont = Typeface.createFromAsset(getAssets(), "fonts/Capture_it.ttf");
+        mAppNameTextView.setTypeface(captureFont);
     }
 }
