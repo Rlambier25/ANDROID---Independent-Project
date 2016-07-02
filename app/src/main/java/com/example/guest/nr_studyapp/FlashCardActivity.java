aef4358125369d141a25186b60ee167312b2c19c
Reed Lambier <rlambier46@gmail.com>
2016-07-01 17:58:44 -0700

    Will add working list view for displaying hard coded array information

--- app/src/main/java/com/example/guest/nr_studyapp/FlashCardActivity.java
+++ app/src/main/java/com/example/guest/nr_studyapp/FlashCardActivity.java
@@ -1,13 +1,27 @@
 package com.example.guest.nr_studyapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class FlashCardActivity extends AppCompatActivity {
+    private ListView mFlashCards;
+    private String[] flashcards = new String[] {"Acidosis", "Acute stress reaction", "Adjuncts", "Aggravation", "Agitation", "Algorithm", "Alkalosis", "Anoxia", "Antecubital", "Antibiotic", "Antibodies", "Antigen", "Antipyretic", "Arthritis", "Aseptic", "Blunt Trauma", "BSIP", "Copius", "Decontamination", "Delayed Stress Reaction", "Dermatology", "Disinfection", "Edema", "Electrodes", "Embolus", "Emphysema", "Epidemiology", "Epistaxis", "Etiology", "Expedite", "Febrile", "Gait", "Geriatrics", "Hemothorax", "Hepatitis", "Hernia", "Infiltration", "Inflammation", "Infusion", "Ischemia"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_flash_card);
+
+        mFlashCards = (ListView) findViewById(R.id.flashCards);
+
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, flashcards);
+        mFlashCards.setAdapter(adapter);
+
     }
 }
