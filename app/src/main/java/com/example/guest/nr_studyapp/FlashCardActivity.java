20e19563591565bf460f055379f5d7b45b79c026
Reed Lambier <rlambier46@gmail.com>
2016-07-01 18:10:57 -0700

    Working list and flashcard click. Added signup button

--- app/src/main/java/com/example/guest/nr_studyapp/FlashCardActivity.java
+++ app/src/main/java/com/example/guest/nr_studyapp/FlashCardActivity.java
@@ -3,8 +3,12 @@ package com.example.guest.nr_studyapp;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
+import android.widget.TextView;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -22,6 +26,14 @@ public class FlashCardActivity extends AppCompatActivity {
 
         ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, flashcards);
         mFlashCards.setAdapter(adapter);
+
+        mFlashCards.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String flashcard = ((TextView)view).getText().toString();
+                Toast.makeText(FlashCardActivity.this, flashcard, Toast.LENGTH_LONG).show();
+            }
+        });
 
     }
 }
