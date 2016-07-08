fd5e5b8e3f1960f7d615891d09863f9ae757a27d
Reed Lambier <rlambier46@gmail.com>
2016-07-07 19:33:40 -0700

    Will add OAuth and SignPost dependencies. Beginning of Constants file. Leaving on lesson API Requests and Responses

---
+++ app/src/main/java/com/example/guest/nr_studyapp/Constants.java
@@ -0,0 +1,10 @@
+package com.example.guest.nr_studyapp;
+
+public class Constants {
+    public static final String QUIZLET_CONSUMER_KEY = BuildConfig.QUIZLET_CONSUMER_KEY;
+
+
+//    this is where I am leaving off. Adding constants for the URL. URL link is in AndroidManifest. Lesson is API Requests and Responses
+//    public static final String YELP_BASE_URL = "https://api.yelp.com/v2/search?term=food";
+//    public static final String YELP_LOCATION_QUERY_PARAMETER = "location";
+}
